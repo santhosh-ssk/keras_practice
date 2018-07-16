086c0ecc1b491582ca84395522d61ea94a2a4c2e
santhsh kumar <clggrievances@gmail.com>
2018-07-16 21:20:29 +0530

    changes

--- simple_rnn/simple_rnn.py
+++ simple_rnn/simple_rnn.py
@@ -17,7 +17,7 @@ text=" ".join(lines)
 chars=set([c for c in text]) 
 nb_chars=len(chars)
 char2index=	dict((c,i) for i,c in enumerate(chars))
-index2chars=	dict((i,c) for i,c in enumerate(chars))
+index2char=	dict((i,c) for i,c in enumerate(chars))
 print(nb_chars)
 
 SEQLEN = 10
@@ -29,10 +29,48 @@ for i in range(0, len(text) - SEQLEN, STEP):
 	label_chars.append(text[i + SEQLEN])
 
 #preparing training data
-import numpy as numpy
-x=np.array((len(input_chars),SEQLEN,nb_chars),dtype=np.bool)
-y=np.array((len(input_chars),nb_chars),dtype=np.bool)
+import numpy as np
+x=np.zeros((len(input_chars),SEQLEN,nb_chars),dtype=np.bool)
+y=np.zeros((len(input_chars),nb_chars),dtype=np.bool)
 for i,input_char in enumerate(input_chars):
 	for j,ch in enumerate(input_char):
 		x[i,j,char2index[ch]]=1
 	y[i,char2index[label_chars[i]]]=1
+    
+#preparing the model
+from keras.layers import Dense, Activation
+from keras.layers.recurrent import SimpleRNN
+from keras.models import Sequential
+HIDDEN_SIZE = 128
+
+BATCH_SIZE = 128
+NUM_ITERATIONS = 25
+NUM_EPOCHS_PER_ITERATION = 1
+NUM_PREDS_PER_EPOCH = 100
+
+model = Sequential()
+model.add(SimpleRNN(HIDDEN_SIZE, return_sequences=False,
+input_shape=(SEQLEN, nb_chars),
+unroll=True))
+model.add(Dense(nb_chars))
+model.add(Activation("softmax"))
+model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
+model.summary()
+for iteration in range(NUM_ITERATIONS):
+    print("=" * 50)
+    print("Iteration #: %d" % (iteration))
+    model.fit(x, y, batch_size=BATCH_SIZE, epochs=NUM_EPOCHS_PER_ITERATION)
+    test_idx = np.random.randint(len(input_chars))
+    test_chars = input_chars[test_idx]
+    print("Generating from seed: %s" % (test_chars))
+    print(test_chars, end="")
+    for i in range(NUM_PREDS_PER_EPOCH):
+        Xtest = np.zeros((1, SEQLEN, nb_chars))
+        for i, ch in enumerate(test_chars):
+            Xtest[0, i, char2index[ch]] = 1
+        pred = model.predict(Xtest, verbose=0)[0]
+        ypred = index2char[np.argmax(pred)]
+        print('(',test_chars,',',test_chars[1:] +ypred, end="),")
+        # move forward with test_chars + ypred
+        test_chars = test_chars[1:] + ypred
+print()
